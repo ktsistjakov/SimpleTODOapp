e3c2018ae0cbf8b7da46e0c96deb87e089edf416
Konstantin Tsistjakov <ktsistjakov@gmail.com>
2022-11-17 12:33:44 +0200

    This is where it all begins...

---
+++ SimpleTODOApp/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  SimpleTODOApp
+//
+//  Created by Konstantin Tsistjakov on 17.11.2022.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
